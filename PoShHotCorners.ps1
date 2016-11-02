a890f7950da11b7824e8d20fedb7a5b8bbab6d74
Beej <beej126@hotmail.com>
2016-11-01 23:59:53 -0700

    Add files via upload

---
+++ PoShHotCorners.ps1
@@ -0,0 +1,76 @@
+# Source: http://www.powershellmagazine.com/2013/07/18/pstip-how-to-switch-off-display-with-powershell/
+
+# Turn display off by calling WindowsAPI.
+ 
+# SendMessage(HWND_BROADCAST,WM_SYSCOMMAND, SC_MONITORPOWER, POWER_OFF)
+# HWND_BROADCAST  0xffff
+# WM_SYSCOMMAND   0x0112
+# SC_MONITORPOWER 0xf170
+# POWER_OFF       0x0002
+ 
+Add-Type -TypeDefinition '
+using System;
+using System.Runtime.InteropServices;
+ 
+namespace Utilities {
+   public static class Display
+   {
+      [DllImport("user32.dll", CharSet = CharSet.Auto)]
+      private static extern IntPtr SendMessage(
+         IntPtr hWnd,
+         UInt32 Msg,
+         IntPtr wParam,
+         IntPtr lParam
+      );
+ 
+      public static void PowerOff ()
+      {
+         SendMessage(
+            (IntPtr)0xffff, // HWND_BROADCAST
+            0x0112,         // WM_SYSCOMMAND
+            (IntPtr)0xf170, // SC_MONITORPOWER
+            (IntPtr)0x0002  // POWER_OFF
+         );
+      }
+   }
+}
+'
+
+#this article got me rolling: http://stackoverflow.com/questions/15505812/why-dont-add-eventname-work-with-timer
+Add-Type -AssemblyName System.Windows.Forms
+
+$notifyIcon = New-Object System.Windows.Forms.NotifyIcon
+$notifyIcon.Icon = New-Object System.Drawing.Icon "$(Split-Path -parent $PSCommandPath)\icon.ico"
+
+$notifyIcon.add_MouseDown( { 
+  if ($script:contextMenu.Visible) { $script:contextMenu.Hide(); return }
+  if ($_.Button -ne [System.Windows.Forms.MouseButtons]::Left) {return}
+
+  #from: http://stackoverflow.com/questions/21076156/how-would-one-attach-a-contextmenustrip-to-a-notifyicon
+  #nugget: ContextMenu.Show() yields a known popup positioning bug... this trick leverages notifyIcons private method that properly handles positioning
+  [System.Windows.Forms.NotifyIcon].GetMethod("ShowContextMenu", [System.Reflection.BindingFlags] "NonPublic, Instance").Invoke($script:notifyIcon, $null)
+})
+
+$contextMenu = New-Object System.Windows.Forms.ContextMenuStrip
+$contextMenu.ShowImageMargin = $false
+$notifyIcon.ContextMenuStrip = $contextMenu
+$contextMenu.Items.Add( "E&xit", $null, { $notifyIcon.Visible = $false; [System.Windows.Forms.Application]::Exit() } ) | Out-Null
+$contextMenu.Show(); $contextMenu.Hide() #just to initialize the window handle to give to $timer.SynchronizingObject below
+
+$timer = New-Object System.Timers.Timer
+$timer.Interval = 500
+$timer.add_Elapsed({
+  $mouse = [System.Windows.Forms.Cursor]::Position
+  $bounds = [System.Windows.Forms.Screen]::FromPoint($mouse).Bounds #thank you! - http://stackoverflow.com/questions/26402955/finding-monitor-screen-on-which-mouse-pointer-is-present
+  #Write-Host "x: $($mouse.x), y:$($mouse.y), width: $($bounds.Width), height: $($bounds.Height), sleep: $($mouse.x -gt $bounds.Width-10 -and $mouse.y -gt $bounds.Height-10)"
+  
+  # here's the beef... currently set to trigger at lower right corner, naturally just season to your own taste (upper left = 0,0)
+  if ($mouse.x -gt $bounds.Width-10 -and $mouse.y -gt $bounds.Height-10) { [Utilities.Display]::PowerOff() } 
+})
+
+#frugally reusing $contextMenu vs firing up another blank form, not really necessary but i was curious if it'd work... the notify icon itself does not implement InvokeRequired
+$timer.SynchronizingObject = $contextMenu
+
+$timer.start()
+$notifyIcon.Visible = $true
+[System.Windows.Forms.Application]::Run()
