4b869345cbbda712471e198bbc28bf133f8dab48
Brent Anderson <BAnderson@KingCounty.gov>
2020-03-14 21:42:06 -0700

    only showing additional blank display X options when there's more than one, otherwise the powerdown is good for one screen

--- MakeShortcut.cmd
+++ MakeShortcut.cmd
@@ -1,18 +1,11 @@
 @ECHO OFF
 SETLOCAL
 
-::**** xxmklink args in order left to right
-::path of the shortcut (.lnk added as needed)
-::path of the object represented by the shortcut
-::argument string (use quotes with space, see below)
-::path of the working directory (for "Start in")
-::description string (shown in Shosrtcut's Properties)
-::display mode (1:Normal [default], 3:Maximized, 7:Minimized)
-::icon file [with optional icon index value n]
-
 :: -new_console:z - overrides cmder/conemu as default shell so that windowstyle hidden works
 
-"%~dp0xxmklink" "%~dp0PoShHotCorners.lnk" PowerShell.exe "-new_console:z -ExecutionPolicy Bypass -WindowStyle Hidden -File ^"%~dp0PoShHotCorners.ps1^"" "" "Windows PowerShell Hot Corners" 7 "%~dp0Icon.ico"
+set shortcutfilename=PoSh Hot Corners
+nircmd shortcut PowerShell.exe "%~dp0" "%shortcutfilename%" "-ExecutionPolicy Bypass -WindowStyle Hidden -File PoShHotCorners.ps1" "%~dp0icon.ico" "" "" "%~dp0"
+
 
 if %errorlevel% neq 0 (
   pause
@@ -25,6 +18,6 @@ echo *******************************************************
 set install=Y
 set /p install="copy shortcut to your auto startup folder [Y/n]: "
 IF %install% equ Y (
-  copy "%~dp0PoShHotCorners.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+  copy "%~dp0%shortcutfilename%.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
   start "" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 )
--- PoShHotCorners.ps1
+++ PoShHotCorners.ps1
@@ -3,7 +3,7 @@
 Add-Type -TypeDefinition '
 using System;
 using System.Runtime.InteropServices;
- 
+
 namespace Utilities {
    public static class Display
    {
@@ -14,7 +14,7 @@ namespace Utilities {
          IntPtr wParam,
          IntPtr lParam
       );
- 
+
       public static void PowerOff ()
       {
          SendMessage(
@@ -39,7 +39,7 @@ function showContextMenu {
   [System.Windows.Forms.NotifyIcon].GetMethod("ShowContextMenu", [System.Reflection.BindingFlags] "NonPublic, Instance").Invoke($script:notifyIcon, $null)
 }
 
-$notifyIcon.add_MouseDown( { 
+$notifyIcon.add_MouseDown( {
   if ($script:contextMenu.Visible) { $script:contextMenu.Hide(); return }
   if ($_.Button -ne [System.Windows.Forms.MouseButtons]::Left) {return}
 
@@ -52,30 +52,32 @@ $contextMenu.ShowImageMargin = $false
 $notifyIcon.ContextMenuStrip = $contextMenu
 
 # create menu entries to blank each screen individually (not a true sleep)
-[System.Windows.Forms.Screen]::AllScreens | % {
-  # create a blank window to selectively blank out one screen without the other
-  $frmBlank =  New-Object System.Windows.Forms.Form
-  $frmBlank.Text = $_.DeviceName.Replace("\\.\", "") 
-  $frmBlank.BackColor = [System.Drawing.Color]::FromName("Black")
-  $frmBlank.ShowInTaskbar = $false
-  $frmBlank.TopMost = $true
-  $frmBlank.Show() #crucial sequence to show and then set bounds or else window top/left don't get set properly
-  $frmBlank.Bounds = $_.Bounds
-  #this prevented bounds restore after sleep - $frmBlank.WindowState = [System.Windows.Forms.FormWindowState]::Maximized
-  $frmBlank.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::None
-  $frmBlank.Hide()
+if ([System.Windows.Forms.Screen]::AllScreens.length -gt 1) {
+    [System.Windows.Forms.Screen]::AllScreens | % {
+    # create a blank window to selectively blank out one screen without the other
+    $frmBlank =  New-Object System.Windows.Forms.Form
+    $frmBlank.Text = $_.DeviceName.Replace("\\.\", "")
+    $frmBlank.BackColor = [System.Drawing.Color]::FromName("Black")
+    $frmBlank.ShowInTaskbar = $false
+    $frmBlank.TopMost = $true
+    $frmBlank.Show() #crucial sequence to show and then set bounds or else window top/left don't get set properly
+    $frmBlank.Bounds = $_.Bounds
+    #this prevented bounds restore after sleep - $frmBlank.WindowState = [System.Windows.Forms.FormWindowState]::Maximized
+    $frmBlank.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::None
+    $frmBlank.Hide()
 
-  $menuItem = $contextMenu.Items.Add("Blank $($frmBlank.Text)")
-  $menuItem.Tag = New-Object -TypeName PSObject -Property @{Form = $frmBlank; Bounds = $_.Bounds}
+    $menuItem = $contextMenu.Items.Add("Blank $($frmBlank.Text)")
+    $menuItem.Tag = New-Object -TypeName PSObject -Property @{Form = $frmBlank; Bounds = $_.Bounds}
 
-  $menuItem.add_Click({
-    $thisForm = $this.Tag.Form
-    @($thisForm.Show,$thisForm.Hide)[$thisForm.Visible].Invoke()
-    $thisForm.Bounds = $this.Tag.Bounds #restore bounds because monitor sleep can shuffle windows to different screens
-    [System.Windows.Forms.Application]::DoEvents() #crucial to allow blank form to cover task bar
-    $thisForm.BringToFront() #also crucial to cover task bar
-    $this.Text = @("Blank","Show")[$thisForm.Visible] + " " + $thisForm.Text
-  })
+    $menuItem.add_Click({
+        $thisForm = $this.Tag.Form
+        @($thisForm.Show,$thisForm.Hide)[$thisForm.Visible].Invoke()
+        $thisForm.Bounds = $this.Tag.Bounds #restore bounds because monitor sleep can shuffle windows to different screens
+        [System.Windows.Forms.Application]::DoEvents() #crucial to allow blank form to cover task bar
+        $thisForm.BringToFront() #also crucial to cover task bar
+        $this.Text = @("Blank","Show")[$thisForm.Visible] + " " + $thisForm.Text
+    })
+    }
 }
 
 $contextMenu.Items.Add( "E&xit", $null, { $notifyIcon.Visible = $false; [System.Windows.Forms.Application]::Exit() } ) | Out-Null
@@ -92,13 +94,13 @@ $timer.add_Elapsed({
   # Here's the BEEF
   <#  __  __              _          __  __            __              ____
      / / / /__  ________ ( )_____   / /_/ /_  ___     / /_  ___  ___  / __/
-    / /_/ / _ \/ ___/ _ \|// ___/  / __/ __ \/ _ \   / __ \/ _ \/ _ \/ /_  
-   / __  /  __/ /  /  __/ (__  )  / /_/ / / /  __/  / /_/ /  __/  __/ __/  
+    / /_/ / _ \/ ___/ _ \|// ___/  / __/ __ \/ _ \   / __ \/ _ \/ _ \/ /_
+   / __  /  __/ /  /  __/ (__  )  / /_/ / / /  __/  / /_/ /  __/  __/ __/
   /_/ /_/\___/_/   \___/ /____/   \__/_/ /_/\___/  /_.___/\___/\___/_/     #>
 
   # mouse/screen coords are based on UPPER LEFT equals X=0,Y=0
   # just expand this to multiple IF blocks to support triggers in additional mouse locations
-  
+
   # the following expression currently triggers at the LOWER RIGHT corner
   # i.e. mouseX within 10px of right edge, mouseY 10px from bottom edge
   if ($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -gt $bounds.Height-10) # lower right corner
@@ -106,7 +108,7 @@ $timer.add_Elapsed({
     # this targets my second screen on the right where the Screen.Bounds.X has a non zero value.
     # see readme.md for brief explanation, or drop me an issue if no worky for your monitor arrangement.
     #-and $bounds.X) {
-    
+
     [Utilities.Display]::PowerOff()
   }
   elseif ($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -lt 10) # upper right corner
@@ -115,7 +117,7 @@ $timer.add_Elapsed({
       & $screenSaverPath
     }
   }
-  
+
   ###################################################################################################
   ### uncomment the following to determine mouse coords for your (multiple) monitor configuration ###
   ###################################################################################################
