c8949a998f9780004841378b1833f3b6f2e51287
Brent Anderson <banderson@kingcounty.gov>
2020-08-09 00:19:04 -0700

    tweaked off display to also windows lock

--- PoShHotCorners.ps1
+++ PoShHotCorners.ps1
@@ -40,12 +40,12 @@ function showContextMenu {
 }
 
 $notifyIcon.add_MouseDown( {
-  if ($script:contextMenu.Visible) { $script:contextMenu.Hide(); return }
-  if ($_.Button -ne [System.Windows.Forms.MouseButtons]::Left) {return}
+    if ($script:contextMenu.Visible) { $script:contextMenu.Hide(); return }
+    if ($_.Button -ne [System.Windows.Forms.MouseButtons]::Left) { return }
 
-  #from: http://stackoverflow.com/questions/21076156/how-would-one-attach-a-contextmenustrip-to-a-notifyicon
-  showContextMenu
-})
+    #from: http://stackoverflow.com/questions/21076156/how-would-one-attach-a-contextmenustrip-to-a-notifyicon
+    showContextMenu
+  })
 
 $contextMenu = New-Object System.Windows.Forms.ContextMenuStrip
 $contextMenu.ShowImageMargin = $false
@@ -53,9 +53,9 @@ $notifyIcon.ContextMenuStrip = $contextMenu
 
 # create menu entries to blank each screen individually (not a true sleep)
 if ([System.Windows.Forms.Screen]::AllScreens.length -gt 1) {
-    [System.Windows.Forms.Screen]::AllScreens | % {
+  [System.Windows.Forms.Screen]::AllScreens | % {
     # create a blank window to selectively blank out one screen without the other
-    $frmBlank =  New-Object System.Windows.Forms.Form
+    $frmBlank = New-Object System.Windows.Forms.Form
     $frmBlank.Text = $_.DeviceName.Replace("\\.\", "")
     $frmBlank.BackColor = [System.Drawing.Color]::FromName("Black")
     $frmBlank.ShowInTaskbar = $false
@@ -67,63 +67,65 @@ if ([System.Windows.Forms.Screen]::AllScreens.length -gt 1) {
     $frmBlank.Hide()
 
     $menuItem = $contextMenu.Items.Add("Blank $($frmBlank.Text)")
-    $menuItem.Tag = New-Object -TypeName PSObject -Property @{Form = $frmBlank; Bounds = $_.Bounds}
+    $menuItem.Tag = New-Object -TypeName PSObject -Property @{Form = $frmBlank; Bounds = $_.Bounds }
 
-    $menuItem.add_Click({
+    $menuItem.add_Click( {
         $thisForm = $this.Tag.Form
-        @($thisForm.Show,$thisForm.Hide)[$thisForm.Visible].Invoke()
+        @($thisForm.Show, $thisForm.Hide)[$thisForm.Visible].Invoke()
         $thisForm.Bounds = $this.Tag.Bounds #restore bounds because monitor sleep can shuffle windows to different screens
         [System.Windows.Forms.Application]::DoEvents() #crucial to allow blank form to cover task bar
         $thisForm.BringToFront() #also crucial to cover task bar
-        $this.Text = @("Blank","Show")[$thisForm.Visible] + " " + $thisForm.Text
-    })
-    }
+        $this.Text = @("Blank", "Show")[$thisForm.Visible] + " " + $thisForm.Text
+      })
+  }
 }
 
 $contextMenu.Items.Add( "E&xit", $null, { $notifyIcon.Visible = $false; [System.Windows.Forms.Application]::Exit() } ) | Out-Null
 
-$screenSaverPath = (Get-ItemProperty "HKCU:\Control Panel\Desktop").{SCRNSAVE.EXE}
+$screenSaverPath = (Get-ItemProperty "HKCU:\Control Panel\Desktop"). { SCRNSAVE.EXE }
 $screenSaverFileName = [System.IO.Path]::GetFileName($screenSaverPath)
 
 $timer = New-Object System.Timers.Timer
 $timer.Interval = 1000
-$timer.add_Elapsed({
-  $mouse = [System.Windows.Forms.Cursor]::Position
-  $bounds = [System.Windows.Forms.Screen]::FromPoint($mouse).Bounds #thank you! - http://stackoverflow.com/questions/26402955/finding-monitor-screen-on-which-mouse-pointer-is-present
+$timer.add_Elapsed( {
+    $mouse = [System.Windows.Forms.Cursor]::Position
+    $bounds = [System.Windows.Forms.Screen]::FromPoint($mouse).Bounds #thank you! - http://stackoverflow.com/questions/26402955/finding-monitor-screen-on-which-mouse-pointer-is-present
 
-  # Here's the BEEF
-  <#  __  __              _          __  __            __              ____
-     / / / /__  ________ ( )_____   / /_/ /_  ___     / /_  ___  ___  / __/
-    / /_/ / _ \/ ___/ _ \|// ___/  / __/ __ \/ _ \   / __ \/ _ \/ _ \/ /_
-   / __  /  __/ /  /  __/ (__  )  / /_/ / / /  __/  / /_/ /  __/  __/ __/
-  /_/ /_/\___/_/   \___/ /____/   \__/_/ /_/\___/  /_.___/\___/\___/_/     #>
+    # Here's the BEEF
+    <#  __  __              _          __  __            __              ____
+       / / / /__  ________ ( )_____   / /_/ /_  ___     / /_  ___  ___  / __/
+      / /_/ / _ \/ ___/ _ \|// ___/  / __/ __ \/ _ \   / __ \/ _ \/ _ \/ /_
+     / __  /  __/ /  /  __/ (__  )  / /_/ / / /  __/  / /_/ /  __/  __/ __/
+    /_/ /_/\___/_/   \___/ /____/   \__/_/ /_/\___/  /_.___/\___/\___/_/     #>
 
-  # mouse/screen coords are based on UPPER LEFT equals X=0,Y=0
-  # just expand this to multiple IF blocks to support triggers in additional mouse locations
+    # mouse/screen coords are based on UPPER LEFT equals X=0,Y=0
+    # just expand this to multiple IF blocks to support triggers in additional mouse locations
 
-  # the following expression currently triggers at the LOWER RIGHT corner
-  # i.e. mouseX within 10px of right edge, mouseY 10px from bottom edge
-  if ($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -gt $bounds.Height-10) # lower right corner
-  {
+    # the following expression currently triggers at the LOWER RIGHT corner
+    # i.e. mouseX within 10px of right edge, mouseY 10px from bottom edge
+
     # this targets my second screen on the right where the Screen.Bounds.X has a non zero value.
     # see readme.md for brief explanation, or drop me an issue if no worky for your monitor arrangement.
-    #-and $bounds.X) {
+    if ($mouse.X - $bounds.X -gt $bounds.Width - 10 -and $mouse.Y -gt $bounds.Height - 10) { # lower right corner
 
-    [Utilities.Display]::PowerOff()
-  }
-  elseif ($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -lt 10) # upper right corner
-  {
-    if (!(get-process $screenSaverFileName -ErrorAction SilentlyContinue)) {
-      & $screenSaverPath
+      # invoke Windows lock screen
+      Invoke-Command { rundll32.exe user32.dll, LockWorkStation }
+
+      # and turn off the display
+      [Utilities.Display]::PowerOff()
     }
-  }
+    elseif ($mouse.X - $bounds.X -gt $bounds.Width - 10 -and $mouse.Y -lt 10) { # upper right corner
+      if (!(get-process $screenSaverFileName -ErrorAction SilentlyContinue)) {
+        & $screenSaverPath
+      }
+    }
 
-  ###################################################################################################
-  ### uncomment the following to determine mouse coords for your (multiple) monitor configuration ###
-  ###################################################################################################
-  # run the ps1 from console to see this output
-  #debug: Write-Host "x: $($mouse.X), y:$($mouse.Y), width: $($bounds.Width), height: $($bounds.Height), trigger: $($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -gt $bounds.Height-10)"
-})
+    ###################################################################################################
+    ### uncomment the following to determine mouse coords for your (multiple) monitor configuration ###
+    ###################################################################################################
+    # run the ps1 from console to see this output
+    #debug: Write-Host "x: $($mouse.X), y:$($mouse.Y), width: $($bounds.Width), height: $($bounds.Height), trigger: $($mouse.X-$bounds.X -gt $bounds.Width-10 -and $mouse.Y -gt $bounds.Height-10)"
+  })
 
 #just to initialize the window handle to give to $timer.SynchronizingObject below
 showContextMenu; $contextMenu.Hide();
